3e8c0a07a7e11d5238dd4359d9c4d5ac28a02e88
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-13 12:58:29 -0600

    Actualizacion estilos de tailwind a css

--- src/components/DebtList.jsx
+++ src/components/DebtList.jsx
@@ -1,25 +1,24 @@
-// src/components/DebtList.jsx
-
-import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebaseConfig";
-import DebtItem from "./DebtItem";
+import { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import DebtItem from './DebtItem';
+import '../styles/DebtList.css'
 
 const DebtList = () => {
   const [deudas, setDeudas] = useState([]);
 
   useEffect(() => {
     const fetchDeudas = async () => {
-      const querySnapshot = await getDocs(collection(db, "deudas"));
+      const querySnapshot = await getDocs(collection(db, 'deudas'));
       setDeudas(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
     };
     fetchDeudas();
   }, []);
 
   return (
-    <div className="max-w-md mx-auto mt-8 mb-11 hola p-4">
-      <h2 className="text-2xl font-bold mb-4">Lista de Deudas</h2>
-      <div className="space-y-4">
+    <div className="debt-list">
+      <h2 className="debt-list-title">Lista de Deudas</h2>
+      <div className="debt-items">
         {deudas.map(deuda => (
           <DebtItem key={deuda.id} deuda={deuda} />
         ))}
@@ -29,5 +28,6 @@ const DebtList = () => {
 };
 
 export default DebtList;
+
 
 
