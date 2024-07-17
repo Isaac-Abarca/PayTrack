8670f2587646d0e1462b490ed602df4bd0988d99
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-16 19:51:40 -0600

    Se agregaron dos nuevos iconos

---
+++ src/assets/AddIcon.jsx
@@ -0,0 +1,8 @@
+const AddIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+      <path d="M208,128a8,8,0,0,1-8,8H136v64a8,8,0,0,1-16,0V136H56a8,8,0,0,1,0-16H120V56a8,8,0,0,1,16,0v64h64A8,8,0,0,1,208,128Z"/>
+    </svg>
+  );
+  
+  export default AddIcon;
+  
