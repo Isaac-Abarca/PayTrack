5fa29f19fa18112a1182f3dba6a3e2e5f66fac03
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-18 15:10:06 -0600

    Se agrega la funcionalidad de logout del sistema

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 // src/components/SettingsModal.jsx
+import { useAuth } from '../contexts/AuthContext';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 import UserIcon from '../assets/UserIconConfig';
 import GearIcon from '../assets/GearIcon';
 import SignOutIcon from '../assets/SignOutIcon';
@@ -7,12 +11,23 @@ import PathIcon from '../assets/PathIcon';
 import "../styles/SettingsModal.css";
 
 const SettingsModal = ({ onClose }) => {
+    const { currentUser } = useAuth();
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+            onClose();
+        } catch (error) {
+            console.error("Error signing out: ", error.message);
+        }
+    };
 
     const handleBackgroundClick = (e) => {
         if (e.target === e.currentTarget) {
             onClose();
         }
     };
+
     return (
         <div className="modal-container" onClick={handleBackgroundClick}>
             <div className="modal-content">
@@ -38,7 +53,7 @@ const SettingsModal = ({ onClose }) => {
                             <PathIcon />
                         </div>
                     </div>
-                    <div className="modal-item">
+                    <div className="modal-item" onClick={handleSignOut}>
                         <div className="modal-item-icon">
                             <SignOutIcon />
                         </div>
