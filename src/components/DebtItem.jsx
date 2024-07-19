839d173faa59434038e16d2ed5fc7abde174e85b
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-19 11:48:55 -0600

    Cambios en el diseño

--- src/components/DebtItem.jsx
+++ src/components/DebtItem.jsx
@@ -1,22 +1,19 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
-import  CurrencyDollarIcon  from '../assets/CurrencyDollarIcon ';
 import '../styles/DebtItem.css'
+import ArrowIcon from '../assets/ArrowIcon';
 
 const DebtItem = ({ deuda }) => {
   return (
     <div className="debt-item">
       <div className="debt-item-info">
-        <div className="icon-container">
-          <CurrencyDollarIcon />
-        </div>
         <div className="debt-details">
-          <p className="debt-title">Deudor: {deuda.deudor}</p>
-          <p className="debt-subtitle">Acreedor: {deuda.acreedor} Monto Inicial: {deuda.montoInicial}</p>
-          <p className="debt-subtitle">Descripción: {deuda.descripcion}</p>
+          <p className="debt-title">{deuda.deudor}</p>
+          <p className="debt-subtitle">${deuda.montoInicial}</p>
+          <p className="debt-subtitle"> {deuda.descripcion}</p>
         </div>
       </div>
-      <Link to={'/debtdetails'} className="debt-link">Ver detalles</Link>
+      <Link to={'/debtdetails'} className="debt-link"><ArrowIcon /></Link>
     </div>
   );
 };
