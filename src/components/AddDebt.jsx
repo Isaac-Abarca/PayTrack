cc3a58addd1348735f2d12b8b6b529b652ef71b4
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-29 17:34:41 -0600

    Cambios basicos en el AddDebt

--- src/components/AddDebt.jsx
+++ src/components/AddDebt.jsx
@@ -1,7 +1,7 @@
 // src/components/AddDebt.jsx
 import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
-import { uploadFile, addDebt } from "../utils/firebaseUtils";
+import { addDebt } from "../utils/firebaseUtils";
 import toast from 'react-hot-toast';
 import ValidatedInput from '../components/ValidatedInput';
 import ValidatedTextarea from '../components/ValidatedTextarea';
@@ -13,32 +13,9 @@ const AddDebt = () => {
   const [acreedor, setAcreedor] = useState("");
   const [montoInicial, setMontoInicial] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState({});
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file && file.type.startsWith("image/")) {
-      setSelectedImage(file);
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePreviewUrl(reader.result);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        file: "Por favor, seleccione una imagen."
-      }));
-    }
-  };
-
-  const handleRemoveImage = () => {
-    setSelectedImage(null);
-    setImagePreviewUrl("");
-  };
 
   const validate = () => {
     const newErrors = {};
@@ -59,10 +36,6 @@ const AddDebt = () => {
     setLoading(true);
 
     try {
-      let fileURL = "";
-      if (selectedImage) {
-        fileURL = await uploadFile(selectedImage, `comprobantes/${selectedImage.name}`);
-      }
 
       await addDebt({
         deudor,
@@ -70,7 +43,6 @@ const AddDebt = () => {
         montoInicial: parseFloat(montoInicial),
         descripcion,
         fechaCreacion: new Date(),
-        comprobante: fileURL,
         userId: currentUser.uid,
         pagos: [] // Asegurar que se crea el array de pagos vacío
       });
@@ -80,12 +52,10 @@ const AddDebt = () => {
       setAcreedor("");
       setMontoInicial("");
       setDescripcion("");
-      setSelectedImage(null);
-      setImagePreviewUrl("");
 
       // Clear errors
       setErrors({});
-      
+
       toast.success('Deuda agregada exitosamente');
     } catch (error) {
       console.error("Error adding document: ", error);
@@ -126,31 +96,6 @@ const AddDebt = () => {
         placeholder="Ingrese una descripción"
         error={errors.descripcion}
       />
-      <div>
-        <div className="input-container">
-          {!imagePreviewUrl && (
-            <label className="input-label">
-              <p className="input-title">Adjuntar Comprobante</p>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-                className="form-input"
-              />
-              {errors.file && <p className="input-error-message">{errors.file}</p>}
-            </label>
-          )}
-          {imagePreviewUrl && (
-            <div>
-              <p className="input-title">Comprobante adjunto</p>
-              <img src={imagePreviewUrl} alt="Preview" style={{ maxWidth: '100%', height: 'auto' }} />
-              <button type="button" onClick={handleRemoveImage} className="mt-2 text-red-500">
-                Eliminar imagen
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
       <div className="btn-container">
         <button type="submit" className="btn-default">
           {loading ? "Guardando..." : "Agregar Deuda"}
