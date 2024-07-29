e3b6a8ebbf7cfa934fa70940f4bc1396888fab78
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2024-07-29 17:40:22 -0600

    Cambios en el navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,26 +12,20 @@ const NavBar = () => {
     <div className="navbar">
       <Link to="/home" className="nav-item active">
         <HomeIcon />
-        {/** <p>Inicio</p>*/}
       </Link>
       <Link to="/listdebs" className="nav-item inactive">
         <CreditCardIcon />
-        {/**<p>Buscar</p> */}
       </Link>
       <Link to="/add-debt" className="nav-item inactive">
         <AddIcon />
-        {/**<p>Agregar</p> */}
 
       </Link>
       <Link to="/notifications" className="nav-item inactive">
         <BellIcon />
-        {/** <p>Notificaciones</p>*/}
 
       </Link>
       <Link to="/chat" className="nav-item inactive">
         <ChatIcon />
-        {/** <p>Chat</p> */}
-
       </Link>
     </div>
   );
